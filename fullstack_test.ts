7749e23f53ae34d7a797e5b851e2b69de2c25a7e
Daniel Flanagan <daniel@lyte.dev>
2023-04-14 14:25:32 -0500

    Add more test cases

--- fullstack_test.ts
+++ fullstack_test.ts
@@ -10,6 +10,12 @@ Deno.test("RPCs successfully with example code", async () => {
     sayHelloTo(a: string) {
       return `Hello, ${a}`;
     },
+    add(a: number, b: number) {
+      return a + b;
+    },
+    report(score: number, goal: number, message: string | null) {
+      return `Your score: ${score} of ${goal} -- ${message || "Thank you!"}`;
+    },
   };
 
   const controller = new AbortController();
@@ -19,6 +25,18 @@ Deno.test("RPCs successfully with example code", async () => {
 
   assertEquals(await client.call("sayHelloTo", ["World"]), "Hello, World");
   assertNotEquals(await client.call("sayHelloTo", ["World"]), "Hello, None");
+
+  assertEquals(await client.call("add", [3, 4]), 7);
+  assertNotEquals(await client.call("add", [3, 3]), 7);
+
+  assertEquals(
+    await client.call("report", [3, 4, "You're welcome!"]),
+    "Your score: 3 of 4 -- You're welcome!",
+  );
+  assertNotEquals(
+    await client.call("report", [3, 9, null]),
+    "Your score: 7 of 9 -- Thank you!",
+  );
 
   controller.abort();
   await listener;
