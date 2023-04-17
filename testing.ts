9dcd7091aac156d065537f7c56587a40b0c79428
Daniel Flanagan <daniel@lyte.dev>
2023-04-17 13:15:37 -0500

    Fix type signatures so we get autocompletion

--- testing.ts
+++ testing.ts
@@ -5,7 +5,7 @@ export async function callFor<T extends Procedures, S extends ProcedureName<T>>(
   rpcs: T,
   p: S,
   args: Parameters<T[S]>,
-) {
+): Promise<ReturnType<T[S]>> {
   const call = await handleProcedureCall(
     buildRequestFor("file:///dev/null", p, args),
     rpcs,
@@ -16,6 +16,6 @@ export async function callFor<T extends Procedures, S extends ProcedureName<T>>(
 export function buildCall<
   T extends Procedures,
   S extends ProcedureName<T>,
->(rpcs: T) {
+>(rpcs: T): (p: S, args: Parameters<T[S]>) => Promise<ReturnType<T[S]>> {
   return async (p: S, args: Parameters<T[S]>) => await callFor(rpcs, p, args);
 }
